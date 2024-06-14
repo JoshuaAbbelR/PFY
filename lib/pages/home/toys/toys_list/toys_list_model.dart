d1b6c600efddac91b586d4303e156962794c25aa
Joshua <abbel56@students.unnes.ac.id>
2024-06-14 13:54:26 +0700

    comit jos 6

--- lib/pages/home/toys/toys_list/toys_list_model.dart
+++ lib/pages/home/toys/toys_list/toys_list_model.dart
@@ -2,12 +2,12 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import 'toys_choose_widget.dart' show ToysChooseWidget;
+import 'toys_list_widget.dart' show ToysListWidget;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-class ToysChooseModel extends FlutterFlowModel<ToysChooseWidget> {
+class ToysListModel extends FlutterFlowModel<ToysListWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
